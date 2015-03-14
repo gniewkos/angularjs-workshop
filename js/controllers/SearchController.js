2a4fc01a79e3945df1f55b19e948614b456575e6
gniewkos <stoigniew.sztank@gmail.com>
2015-03-14 13:12:46 +0100

    Basic elasticsearch search

--- js/controllers/SearchController.js
+++ js/controllers/SearchController.js
@@ -6,30 +6,36 @@
 // It also requires the esFactory to that it can check for a specific type of
 // error which might come back from the client
 SearchApp.controller('SearchController', function ($scope, esClient, esFactory) {
+  $scope.searchText = '';
 
-  esClient.cluster.state({
-    metric: [
-      'cluster_name',
-      'nodes',
-      'master_node',
-      'version'
-    ]
-  })
-  .then(function (resp) {
-    $scope.clusterState = resp;
-    $scope.error = null;
-  })
-  .catch(function (err) {
-    $scope.clusterState = null;
-    $scope.error = err;
+  $scope.search = function() {
 
-    // if the err is a NoConnections error, then the client was not able to
-    // connect to elasticsearch. In that case, create a more detailed error
-    // message
-    if (err instanceof esFactory.errors.NoConnections) {
-      $scope.error = new Error('Unable to connect to elasticsearch. ' +
-        'Make sure that it is running and listening at http://localhost:9200');
-    }
-  });
+    esClient.search({
+      index: 'stories',
+      body: {
+        query: {
+          match_phrase: {
+            body: $scope.searchText
+          }
+        }
+      }
+    })
+    .then(function (resp) {
+      $scope.stories = resp;
+      $scope.error = null;
+    })
+    .catch(function (err) {
+      $scope.stories = null;
+      $scope.error = err;
+
+      // if the err is a NoConnections error, then the client was not able to
+      // connect to elasticsearch. In that case, create a more detailed error
+      // message
+      if (err instanceof esFactory.errors.NoConnections) {
+        $scope.error = new Error('Unable to connect to elasticsearch. ' +
+          'Make sure that it is running and listening at http://localhost:9200');
+      }
+    });
+  }
 
 });
